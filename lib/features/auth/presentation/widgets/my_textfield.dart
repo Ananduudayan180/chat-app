65892272a0de6d76e9c11b22275624cf29ce7ac3
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-13 12:16:33 +0530

    feat: add MyTextField widget

---
+++ lib/features/auth/presentation/widgets/my_textfield.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final String hintText;
+  final bool obscureText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.obscureText = false,
+    required this.validator,
+    this.prefixIcon,
+    this.suffixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 6),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        style: TextStyle(fontSize: 15),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: theme.colorScheme.surface,
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
+          prefixIconConstraints: BoxConstraints(maxHeight: 32, minWidth: 32),
+          hintText: hintText,
+          hintStyle: TextStyle(
+            fontSize: 15,
+            color: theme.inputDecorationTheme.hintStyle!.color!,
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(5),
+          ),
+        ),
+        validator: validator,
+      ),
+    );
+  }
+}
