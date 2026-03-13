601713e812ec671efebd2cb11fe0701972774297
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-13 21:52:55 +0530

    feat: create reusable AuthInputFields widget for text fields

---
+++ lib/features/auth/presentation/widgets/auth_input_fields.dart
@@ -0,0 +1,54 @@
+import 'package:chat_app/features/auth/presentation/widgets/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class AuthInputFields extends StatelessWidget {
+  final TextEditingController? nameController;
+  final TextEditingController emailController;
+  final TextEditingController passController;
+
+  const AuthInputFields({
+    super.key,
+    this.nameController,
+    required this.emailController,
+    required this.passController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: SizedBox(
+        child: Column(
+          children: [
+            nameController != null ? Row(children: [Text("Name")]) : SizedBox(),
+            nameController != null
+                //Name Textfield
+                ? MyTextField(
+                    controller: nameController!,
+                    hintText: 'Name',
+                    validator: null,
+                  )
+                : SizedBox(),
+
+            Row(children: [Text("Email")]),
+            //Email Textfield
+            MyTextField(
+              controller: emailController,
+              hintText: 'Example@gmail.com',
+              validator: null,
+            ),
+
+            Row(children: [Text("Password")]),
+            //Pass Textfield
+            MyTextField(
+              controller: passController,
+              hintText: 'Password',
+              obscureText: true,
+              validator: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
