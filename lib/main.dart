b5fa0b7d12a4da148ff2c266d1bb5dff08b0ab36
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-14 17:30:29 +0530

    feat: add blocProvider in app root

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:chat_app/core/config/firebase_options.dart';
-import 'package:chat_app/core/theme/dark_mode.dart';
+import 'package:chat_app/core/theme/light_mode.dart';
+import 'package:chat_app/features/auth/data/repo/auth_repo_impl.dart';
+import 'package:chat_app/features/auth/data/service/auth_service.dart';
+import 'package:chat_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:chat_app/features/auth/presentation/pages/register_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    BlocProvider<AuthBloc>(
+      create: (context) =>
+          AuthBloc(authRepo: AuthRepoImpl(authService: AuthService())),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: darkMode,
+      theme: lightMode,
       home: RegisterPage(),
     );
   }
