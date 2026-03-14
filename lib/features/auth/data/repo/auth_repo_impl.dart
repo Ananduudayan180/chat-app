3f55db0cb30d4107fa1d59ba1d37643434ff9ea9
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-14 17:11:22 +0530

    feat: implement AuthRepo in AuthRepoImpl

---
+++ lib/features/auth/data/repo/auth_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:chat_app/features/auth/data/service/auth_service.dart';
+import 'package:chat_app/features/auth/domain/entity/user_model.dart';
+import 'package:chat_app/features/auth/domain/repo/auth_repo.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  final AuthService _authService;
+
+  AuthRepoImpl({required AuthService authService}) : _authService = authService;
+  @override
+  Future<UserModel?> loginUser(String email, String pw) =>
+      _authService.loginUser(email, pw);
+
+  @override
+  Future<UserModel?> registerUser(String name, String email, String pw) =>
+      _authService.registerUser(name, email, pw);
+
+  @override
+  Future<void> logoutUser() => _authService.logoutUser();
+}
