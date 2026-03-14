9ca8aed9f69ab737dba7d5b27bc97372c61d819f
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-14 11:30:43 +0530

    feat: add RegisterBox widget for registration

--- lib/features/auth/presentation/widgets/register_box.dart
+++ lib/features/auth/presentation/widgets/register_box.dart
@@ -2,12 +2,13 @@ import 'package:chat_app/features/auth/presentation/widgets/auth_input_fields.da
 import 'package:chat_app/features/auth/presentation/widgets/my_button.dart';
 import 'package:flutter/material.dart';
 
-class LoginBox extends StatelessWidget {
+class RegisterBox extends StatelessWidget {
+  final TextEditingController nameController;
   final TextEditingController emailController;
   final TextEditingController pwController;
-
-  const LoginBox({
+  const RegisterBox({
     super.key,
+    required this.nameController,
     required this.emailController,
     required this.pwController,
   });
@@ -16,14 +17,24 @@ class LoginBox extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     return Container(
-      height: 550,
       color: Theme.of(context).scaffoldBackgroundColor,
+      height: 550,
+      width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Login", style: TextStyle(fontSize: 55)),
           Text(
-            'Sign in to continue',
+            "Create a new\nAccount",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 45,
+              height: 1.1,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'Sign up to continue',
             style: TextStyle(color: theme.colorScheme.primary),
           ),
           SizedBox(height: 40),
@@ -31,24 +42,25 @@ class LoginBox extends StatelessWidget {
           AuthInputFields(
             emailController: emailController,
             passController: pwController,
+            nameController: nameController,
           ),
           //Login button
-          MyButton(buttonName: 'Log in', onTap: () {}),
+          MyButton(buttonName: 'Sign up', onTap: () {}),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "Don't have an account?",
+                'Already registered?',
                 style: TextStyle(
-                  color: theme.inputDecorationTheme.hintStyle!.color!,
+                  color: theme.inputDecorationTheme.hintStyle!.color,
                 ),
               ),
               SizedBox(width: 4),
-              //Navigate -> Register
+              //Navigate -> Login
               GestureDetector(
                 onTap: () {},
                 child: Text(
-                  "Sign up",
+                  'Log in here',
                   style: TextStyle(color: theme.colorScheme.primary),
                 ),
               ),
