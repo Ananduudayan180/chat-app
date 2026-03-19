f131085b3a15fd23a68ec3a31c076947e6aa4d71
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-19 17:08:06 +0530

    feat(nav): add HomeRoute for page switching

---
+++ lib/app/home_route.dart
@@ -0,0 +1,37 @@
+import 'package:chat_app/core/widgets/my_bottom_bar.dart';
+import 'package:chat_app/features/chat/presentation/pages/chat_list_page.dart';
+import 'package:chat_app/features/profile/presentation/pages/profile_page.dart';
+import 'package:chat_app/features/settings/presentation/pages/settings_page.dart';
+import 'package:flutter/material.dart';
+
+class HomeRoute extends StatefulWidget {
+  const HomeRoute({super.key});
+
+  @override
+  State<HomeRoute> createState() => _HomeRouteState();
+}
+
+int currentIndex = 0;
+
+class _HomeRouteState extends State<HomeRoute> {
+  final List<Widget> pages = [
+    const ChatListPage(),
+    const SettingsPage(),
+    const ProfilePage(),
+  ];
+  //switch pages
+  void switchPages(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //my bottom bar
+      bottomNavigationBar: MyBottomBar(onTap: switchPages),
+      body: pages[currentIndex],
+    );
+  }
+}
