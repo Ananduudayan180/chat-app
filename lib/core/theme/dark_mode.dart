303ed4edd2d76546de59ba5a1346cdaf23ce23b9
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-19 14:53:21 +0530

    feat(theme): add light and dark ThemeData

---
+++ lib/core/theme/dark_mode.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData darkMode = ThemeData(
+  textTheme: GoogleFonts.poppinsTextTheme().apply(bodyColor: Colors.white),
+  scaffoldBackgroundColor: const Color(0xFF1A1A1A),
+  colorScheme: const ColorScheme.dark(
+    primary: Color(0xFFFF7700),
+    surface: Colors.white10,
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    hintStyle: TextStyle(color: Colors.white70),
+  ),
+  snackBarTheme: SnackBarThemeData(
+    backgroundColor: Color(0xFF2C2C2C),
+    contentTextStyle: GoogleFonts.poppins(color: Color(0xFFFFFFFF)),
+  ),
+);
---
+++ lib/core/theme/light_mode.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+ThemeData lightMode = ThemeData(
+  textTheme: GoogleFonts.poppinsTextTheme().apply(bodyColor: Colors.grey[900]),
+  scaffoldBackgroundColor: const Color(0xFFF2F2F2),
+  colorScheme: const ColorScheme.light(
+    primary: Color(0xFFFF7700),
+    surface: Colors.white,
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    hintStyle: TextStyle(color: Colors.grey[600]),
+  ),
+  snackBarTheme: SnackBarThemeData(
+    backgroundColor: Color(0xFFE0E0E0),
+    contentTextStyle: GoogleFonts.poppins(color: Color(0xFF000000)),
+  ),
+);
