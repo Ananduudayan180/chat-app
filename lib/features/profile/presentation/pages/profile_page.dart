6180208cd320f16ac8c4a4c2bdd8df696b60d588
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-21 21:00:11 +0530

    feat: implement profile UI

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/features/profile/presentation/widgets/profile_card.dart';
+import 'package:chat_app/features/profile/presentation/widgets/profile_settings_box.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -5,6 +7,20 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Profile Page')));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //profile card
+            ProfileCard(viewPic: () {}, changePic: () {}),
+            //profile settings container
+            ProfileSettingsBox(),
+          ],
+        ),
+      ),
+    );
   }
 }
