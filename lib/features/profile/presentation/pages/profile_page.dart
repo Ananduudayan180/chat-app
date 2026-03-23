050c5957a7d85ac818cdee181ed6699e2789f54e
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-23 17:27:11 +0530

    feat(profile): add blocConsumer and manage Profile state

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,26 +1,64 @@
+import 'package:chat_app/features/auth/data/service/auth_service.dart';
+import 'package:chat_app/features/profile/presentation/bloc/profile_bloc.dart';
 import 'package:chat_app/features/profile/presentation/widgets/profile_card.dart';
 import 'package:chat_app/features/profile/presentation/widgets/profile_settings_box.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+final AuthService _auth = AuthService();
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  void initState() {
+    super.initState();
+    final uid = _auth.currentUserUid;
+    context.read<ProfileBloc>().add(FetchProfileRequested(uid: uid));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            //profile card
-            ProfileCard(viewPic: () {}, changePic: () {}),
-            //profile settings container
-            ProfileSettingsBox(),
-          ],
-        ),
-      ),
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      listener: (context, state) {
+        //Error
+        if (state is ProfileError) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.errorMsg)));
+        }
+      },
+      builder: (context, state) {
+        //Loaded
+        if (state is ProfileLoaded) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //profile card
+                  ProfileCard(
+                    viewPic: () {},
+                    changePic: () {},
+                    profile: state.profile,
+                  ),
+                  //profile settings container
+                  ProfileSettingsBox(),
+                ],
+              ),
+            ),
+          );
+        }
+        //Loading or Initial
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
--- lib/features/profile/presentation/widgets/profile_card.dart
+++ lib/features/profile/presentation/widgets/profile_card.dart
@@ -1,13 +1,16 @@
+import 'package:chat_app/features/profile/domain/entity/profile_model.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfileCard extends StatelessWidget {
   final VoidCallback viewPic;
   final VoidCallback changePic;
+  final ProfileModel profile;
   const ProfileCard({
     super.key,
     required this.viewPic,
     required this.changePic,
+    required this.profile,
   });
 
   @override
@@ -79,13 +82,10 @@ class ProfileCard extends StatelessWidget {
         ),
         SizedBox(height: 12),
         Text(
-          'Anandu Udayan',
+          profile.name,
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        Text(
-          'anandu180@gmail.com',
-          style: TextStyle(color: theme.dividerColor),
-        ),
+        Text(profile.email, style: TextStyle(color: theme.dividerColor)),
       ],
     );
   }
