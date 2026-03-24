a90125b22dc113b7b35fb22ef4c8afbcd11eac44
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-24 15:18:16 +0530

    chore: init supabase in main

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,18 @@ import 'package:chat_app/features/profile/presentation/bloc/profile_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //supabase
+  await Supabase.initialize(
+    url: 'https://ulwvmxsnfnaddbwnssvf.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsd3ZteHNuZm5hZGRid25zc3ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNDQ2MTgsImV4cCI6MjA4OTkyMDYxOH0.XVpRoCotHVazq82qMLsW_0KZtdZcKnmPbiYTMiiwjok',
+  );
   runApp(
     MultiBlocProvider(
       providers: [
