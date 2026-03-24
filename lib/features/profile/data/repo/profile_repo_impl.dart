114ed4aa896318d057621360df5064c1061de951
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-24 20:20:22 +0530

    feat(profile): add uploadProfileImage to repo and implement in repoImpl

--- lib/features/profile/data/repo/profile_repo_impl.dart
+++ lib/features/profile/data/repo/profile_repo_impl.dart
@@ -1,15 +1,33 @@
+import 'dart:typed_data';
 import 'package:chat_app/features/profile/data/service/profile_service.dart';
+import 'package:chat_app/features/profile/data/service/profile_storage_service.dart';
 import 'package:chat_app/features/profile/domain/entity/profile_model.dart';
 import 'package:chat_app/features/profile/domain/repo/profile_repo.dart';
 
 class ProfileRepoImpl implements ProfileRepo {
   final ProfileService _profileService;
+  final ProfileStorageService _storageService;
 
-  ProfileRepoImpl({required ProfileService profileService})
-    : _profileService = profileService;
+  ProfileRepoImpl({
+    required ProfileService profileService,
+    required ProfileStorageService storageService,
+  }) : _storageService = storageService,
+       _profileService = profileService;
 
   //fetch user profile
   @override
   Future<ProfileModel?> fetchUserProfile(String uid) =>
       _profileService.fetchUserProfile(uid);
+
+  //upload profile image
+  @override
+  Future<String> uploadProfileImage({
+    Uint8List? bytes,
+    String? path,
+    required String fileName,
+  }) => _storageService.uploadProfileImage(
+    fileName: fileName,
+    bytes: bytes,
+    path: path,
+  );
 }
--- lib/features/profile/domain/repo/profile_repo.dart
+++ lib/features/profile/domain/repo/profile_repo.dart
@@ -1,5 +1,11 @@
+import 'dart:typed_data';
 import 'package:chat_app/features/profile/domain/entity/profile_model.dart';
 
 abstract class ProfileRepo {
   Future<ProfileModel?> fetchUserProfile(String uid);
+  Future<String> uploadProfileImage({
+    Uint8List? bytes,
+    String? path,
+    required String fileName,
+  });
 }
