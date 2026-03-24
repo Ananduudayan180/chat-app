3318ee8651a388e64a6b2258ffe0f6e3892ef25f
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-24 21:48:30 +0530

    feat(profile): add UploadProfileImage event

--- lib/features/profile/presentation/bloc/profile_event.dart
+++ lib/features/profile/presentation/bloc/profile_event.dart
@@ -7,3 +7,17 @@ final class FetchProfileRequested extends ProfileEvent {
 
   FetchProfileRequested({required this.uid});
 }
+
+final class UploadProfileImage extends ProfileEvent {
+  final Uint8List? bytes;
+  final String? path;
+  final String fileName;
+  final String uid;
+
+  UploadProfileImage({
+    required this.bytes,
+    required this.path,
+    required this.fileName,
+    required this.uid,
+  });
+}
