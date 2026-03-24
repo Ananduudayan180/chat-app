afbef44cc9df57c26e2ab005889f225b43465510
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-24 19:53:14 +0530

    feat(profile): add uploadProfileImage function in Profile storage service

---
+++ lib/features/profile/data/service/profile_storage_service.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class ProfileStorageService {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  Future<String> uploadProfileImage({
+    Uint8List? bytes,
+    String? path,
+    required String fileName,
+  }) async {
+    try {
+      if (path != null) {
+        return await uploadImagePath(fileName, path, 'profile_images');
+      } else if (bytes != null) {
+        return await uploadImageBytes(fileName, bytes, 'profile_images');
+      } else {
+        throw Exception('No image data');
+      }
+    } on Exception catch (e) {
+      throw Exception('Upload failed $e');
+    }
+  }
+
+  //HELPER FUNCTIONS
+  //mobile - desktop upload
+  Future<String> uploadImagePath(
+    String fileName,
+    String path,
+    String bucketId,
+  ) async {
+    File file = File(path);
+    await _supabase.storage
+        .from(bucketId)
+        .upload(fileName, file, fileOptions: const FileOptions(upsert: true));
+    final url = _supabase.storage.from(bucketId).getPublicUrl(fileName);
+    return url;
+  }
+
+  //web upload
+  Future<String> uploadImageBytes(
+    String fileName,
+    Uint8List bytes,
+    String bucketId,
+  ) async {
+    await _supabase.storage
+        .from(bucketId)
+        .uploadBinary(
+          fileName,
+          bytes,
+          fileOptions: const FileOptions(upsert: true),
+        );
+    final url = _supabase.storage.from(bucketId).getPublicUrl(fileName);
+    return url;
+  }
+}
