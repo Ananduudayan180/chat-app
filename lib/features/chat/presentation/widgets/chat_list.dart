9fec175c905ca540c44a6bb74997a2ebd29e4c54
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-26 12:57:16 +0530

    feat(chat): implement dummy chat list ui

---
+++ lib/features/chat/presentation/widgets/chat_list.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ChatList extends StatelessWidget {
+  const ChatList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Expanded(
+      child: ListView.separated(
+        itemBuilder: (context, index) {
+          //list tile
+          return ListTile(
+            leading: CircleAvatar(radius: 25),
+            title: Text('Sara Sanders'),
+            subtitle: Text(
+              'Hi..How are you?',
+              style: TextStyle(color: theme.dividerColor),
+            ),
+            trailing: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '12:33',
+                  style: TextStyle(
+                    color: theme.colorScheme.primary,
+                    fontSize: 12,
+                  ),
+                ),
+                SizedBox(height: 5),
+                //notification container
+                Container(
+                  width: 20,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.red, Colors.orangeAccent],
+                      begin: AlignmentGeometry.bottomLeft,
+                      end: AlignmentGeometry.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+
+                  child: Center(
+                    child: Text(
+                      '3',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(indent: 78, thickness: 0.1);
+        },
+        itemCount: 15,
+      ),
+    );
+  }
+}
