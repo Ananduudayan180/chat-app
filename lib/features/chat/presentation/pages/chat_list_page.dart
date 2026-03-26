2994357520180f3fd4c3dfc1f91347a2b0cc6383
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-26 13:04:59 +0530

    feat(chat): implement ChatListPage dummy ui

--- lib/features/chat/presentation/pages/chat_list_page.dart
+++ lib/features/chat/presentation/pages/chat_list_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/core/widgets/my_textfield.dart';
+import 'package:chat_app/features/chat/presentation/widgets/chat_list.dart';
 import 'package:flutter/material.dart';
 
 class ChatListPage extends StatelessWidget {
@@ -5,6 +7,35 @@ class ChatListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Chat List Page')));
+    final searchController = TextEditingController();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            //title & text field
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //title
+                const Text(
+                  'Chatify',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                //my text field
+                MyTextField(
+                  controller: searchController,
+                  hintText: 'Search',
+                  validator: null,
+                ),
+              ],
+            ),
+          ),
+          //chat list
+          ChatList(),
+        ],
+      ),
+    );
   }
 }
