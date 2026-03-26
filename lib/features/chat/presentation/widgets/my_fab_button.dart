27c83620f81463924eb94ce73f6d1a1b708fb42e
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-26 17:55:43 +0530

    feat(chat): add custom fab button

---
+++ lib/features/chat/presentation/widgets/my_fab_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyFabButton extends StatelessWidget {
+  const MyFabButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return FloatingActionButton(
+      onPressed: () {},
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        //decaration
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.red, Colors.orange],
+            begin: AlignmentGeometry.bottomLeft,
+            end: AlignmentGeometry.bottomRight,
+          ),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(15),
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+          ),
+        ),
+        child: Center(
+          //icon
+          child: FaIcon(
+            FontAwesomeIcons.solidMessage,
+            color: theme.iconTheme.color,
+          ),
+        ),
+      ),
+    );
+  }
+}
