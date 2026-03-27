03a69f10b93f878a2f47a07c6e4119c9d6cd9232
Anandu Udayan <anandhuudayan180@gamil.com>
2026-03-27 20:35:01 +0530

    feat(users): add AppUserModel

---
+++ lib/features/users/domain/entity/app_user_model.dart
@@ -0,0 +1,11 @@
+class AppUserModel {
+  final String uid;
+  final String name;
+  final String profileImageUrl;
+
+  AppUserModel({
+    required this.uid,
+    required this.name,
+    required this.profileImageUrl,
+  });
+}
