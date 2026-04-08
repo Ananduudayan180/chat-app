c81a208f21a3bdd2efd6478c09a33260697e2928
Anandu Udayan <anandhuudayan180@gamil.com>
2026-04-08 21:28:10 +0530

    feat(chat): add stremMessages in ChatRepo and ChatRepoImpl

--- lib/features/chat/data/repo/chat_repo_impl.dart
+++ lib/features/chat/data/repo/chat_repo_impl.dart
@@ -1,13 +1,24 @@
 import 'package:chat_app/features/chat/data/service/chat_service.dart';
+import 'package:chat_app/features/chat/data/service/message_service.dart';
 import 'package:chat_app/features/chat/domain/entity/chat_model.dart';
+import 'package:chat_app/features/chat/domain/entity/message_model.dart';
 import 'package:chat_app/features/chat/domain/repo/chat_repo.dart';
 
 class ChatRepoImpl extends ChatRepo {
   final ChatService _chatService;
+  final MessageService _messageService;
 
-  ChatRepoImpl({required ChatService chatService}) : _chatService = chatService;
+  ChatRepoImpl({
+    required ChatService chatService,
+    required MessageService messageService,
+  }) : _messageService = messageService,
+       _chatService = chatService;
 
   @override
   Stream<List<ChatModel>> streamChats(String currentUserUid) =>
       _chatService.streamChats(currentUserUid);
+
+  @override
+  Stream<List<MessageModel>> streamMessages(String chatId) =>
+      _messageService.streamMessages(chatId);
 }
--- lib/features/chat/domain/repo/chat_repo.dart
+++ lib/features/chat/domain/repo/chat_repo.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/features/chat/domain/entity/chat_model.dart';
+import 'package:chat_app/features/chat/domain/entity/message_model.dart';
 
 abstract class ChatRepo {
   Stream<List<ChatModel>> streamChats(String currentUserUid);
+  Stream<List<MessageModel>> streamMessages(String chatId);
 }
