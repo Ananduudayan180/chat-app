fc7bf6f824c6c4d22a3d1aff514d2c70f979c32b
Anandu Udayan <anandhuudayan180@gamil.com>
2026-04-10 20:29:31 +0530

    feat(theme): add ThemeBloc builder to app level

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,23 @@
 import 'package:chat_app/core/theme/dark_mode.dart';
+import 'package:chat_app/core/theme/light_mode.dart';
 import 'package:chat_app/features/auth/presentation/pages/auth_switcher.dart';
+import 'package:chat_app/features/theme/presentation/bloc/theme_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: darkMode,
-      home: AuthSwitcher(),
+    return BlocBuilder<ThemeBloc, ThemeState>(
+      builder: (context, state) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: state.isDark ? darkMode : lightMode,
+          home: AuthSwitcher(),
+        );
+      },
     );
   }
 }
