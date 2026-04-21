8ac7e84926abf0fe873e4aa3eec779d9104073cc
Anandu Udayan <anandhuudayan180@gamil.com>
2026-04-21 11:46:32 +0530

    feat(users): update FetchUsers event to include currentUserUid and filter out blocked users

--- lib/features/users/presentation/bloc/users/users_event.dart
+++ lib/features/users/presentation/bloc/users/users_event.dart
@@ -3,4 +3,8 @@ part of 'users_bloc.dart';
 @immutable
 sealed class UsersEvent {}
 
-final class FetchUsers extends UsersEvent {}
+final class FetchUsers extends UsersEvent {
+  final String currentUserUid;
+
+  FetchUsers({required this.currentUserUid});
+}
