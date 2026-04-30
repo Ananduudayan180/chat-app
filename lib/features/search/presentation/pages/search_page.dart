7a5f63082fb2acd72d8e9a80bfe192314cd8acfc
Anandu Udayan <anandhuudayan180@gamil.com>
2026-04-30 21:01:11 +0530

    feat(search): add BlocConsumer to SearchPage with search functionality

--- lib/features/search/presentation/pages/search_page.dart
+++ lib/features/search/presentation/pages/search_page.dart
@@ -1,5 +1,8 @@
 import 'package:chat_app/core/widgets/my_textfield.dart';
+import 'package:chat_app/features/search/presentation/bloc/search_bloc.dart';
+import 'package:chat_app/features/users/presentation/widgets/users_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SearchPage extends StatefulWidget {
@@ -12,43 +15,73 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   final searchController = TextEditingController();
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //appbar
-        AppBar(
-          backgroundColor: Colors.transparent,
-          title: Text(
-            'Search',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            children: [
-              //search textfield
-              MyTextField(
-                controller: searchController,
-                hintText: 'Search',
-                validator: null,
-              ),
-              //svg
-              Center(
-                heightFactor: 2,
-                child: Column(
-                  children: [
-                    SvgPicture.asset('assets/svg/search.svg', width: 250),
-                    Text('Find users', style: TextStyle(color: Colors.grey)),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+  void initState() {
+    super.initState();
+    searchController.addListener(searchQuery);
+  }
 
-        //svg
-      ],
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  void searchQuery() {
+    context.read<SearchBloc>().add(SearchUsers(query: searchController.text));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.transparent,
+            title: Text(
+              'Search',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          //search textfield
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: MyTextField(
+              controller: searchController,
+              hintText: 'Search',
+              validator: null,
+            ),
+          ),
+          BlocConsumer<SearchBloc, SearchState>(
+            listener: (context, state) {
+              if (state is SearchError) {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text(state.errorMsg)));
+              }
+            },
+            builder: (context, state) {
+              if (state is SearchLoading) {
+                return Center(child: CircularProgressIndicator());
+              }
+              if (state is SearchLoaded) {
+                return Expanded(child: UsersListView(users: state.users));
+              }
+              return Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset('assets/svg/search.svg', width: 250),
+                      Text('Find users', style: TextStyle(color: Colors.grey)),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
